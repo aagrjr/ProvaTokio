b1c09f88639a8e6c07375faf69dca5d18d8ac122
aagrjr <44318148+aagrjr@users.noreply.github.com>
2018-10-22 09:02:58 -0300

    Ultima versão
    Atualização para contemplar filas

--- DevTokio/src/main/java/com/tokio/devtokio/resource/AutorResource.java
+++ DevTokio/src/main/java/com/tokio/devtokio/resource/AutorResource.java
@@ -8,6 +8,8 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,15 +26,32 @@ public class AutorResource {
 	@Autowired
 	private AutorRepository autorRepository;
 
+	@Autowired 
+	private JmsTemplate jmsTemplate;	
 	
 	@PostMapping
-	public ResponseEntity<Autor> save(@Valid @RequestBody Autor autor, HttpServletResponse response){
-		Autor autorSaved = this.autorRepository.save(autor);
-		return ResponseEntity.status(HttpStatus.CREATED).body(autorSaved);
+	public ResponseEntity<String> save(@Valid @RequestBody Autor autor, HttpServletResponse response){
+		try {
+			jmsTemplate.convertAndSend("queue.autor",autor);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("NOk");
+		}
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body("Ok");
 	}
 	
 	@GetMapping
 	public List<Autor> findAll(){
 		return this.autorRepository.findAll();
 	}
+	
+	@JmsListener(destination = "queue.autor")
+	public void onReceiverQueue(Autor autor) {
+		try {
+			this.autorRepository.save(autor);
+			System.out.println(autor.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}	
 }
